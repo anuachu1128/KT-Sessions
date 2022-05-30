29e490269c591485f953bd18208e009c592157f6
anuachu1128 <65966983+anuachu1128@users.noreply.github.com>
2022-05-30 19:30:41 +0530

    Remove some comments to improve readability

--- KT Session 1/Assignment/KT_Task1.py
+++ KT Session 1/Assignment/KT_Task1.py
@@ -9,21 +9,17 @@ import json
 #1. SUMMARY
 def videoSummary(videoPath):
     #Component 1: Video Path - Taken as argument
-    #Taking the name of the video after splitting by '/'
     videoFileName = videoPath.split('/')[-1]
     #Getting components of the path seperated by '_' after removing .mp4 extension
     videoPathComp = videoFileName.strip('.mp4').split('_')
     #Component 2: Environment
-    #Checking whether the second letter of the first comp is H or S 
     if(videoPathComp[0][1] == 'H'):
         env="home"
     else:
         env="studio"
     #Component 3: Signer ID
-    #Returning int of the second letter of second component
     signerID = int(videoPathComp[1][1])
     #Component 4: Gloss ID
-    #Returning int of the third component
     glossID = int(videoPathComp[2])
     #Component 5: Position
     if(videoPathComp[0][1] == 'H'):
@@ -54,25 +50,21 @@ def videoSummary(videoPath):
     data.append(['width',videoWidth])
     data.append(['rgb_path',sys.argv[2]+videoPath.split('/')[-1].strip('.mp4')+'_lowResRGB.avi' ])
     data.append(['pose_path', sys.argv[2]+videoPath.split('/')[-1].strip('.mp4')+'_poseEstimate.json'])
-    #Creating a dataframe using the list
+    #Creating a dataframe using the list and converting to csv
     df = pd.DataFrame(data)
-    #Converting dataframe to a csv file
     df.to_csv(videoPath.split('/')[-1].strip('.mp4')+'_Summary.csv',index=False, header=False)
 
 #2. LOW RESOLUTION RGB SQUARE VIDEO
 def resizeAndCrop(videoPath):
-    #Creating the video capture object for the original video
     videoElem = cv2.VideoCapture(videoPath.split('/')[-1])
     #Saving the resized and cropped video in the path specified by the user taking in -input video file, codec to compress frames using FourCC code, framerate (fps), resolution
     out = cv2.VideoWriter(sys.argv[2]+videoPath.split('/')[-1].strip('.mp4')+'_lowResRGB.avi',cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), float(videoElem.get(cv2.CAP_PROP_FPS)), (320,320))
-    #Reading frames of the orginal video
     while True:
         #Taking each frame of the video
         success, frame = videoElem.read()
         if success == True:
             #Resizing the original frame to 320x320 resolution with no scaling on x (fx) and y (fy) axis using bicubic interpolation
             resizedFrame = cv2.resize(frame,(320,320),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
-            #Writing the resized frame to the output video file
             out.write(resizedFrame)
         else:
             break
@@ -95,7 +87,6 @@ def retrievePoseEstimates(videoPath):
     while True:
         success, frame = videoElem.read()
         if success == True:
-            #Recolor the frame from BGR to RGB to be passed on to Holistic model, since using opencv, feed is obtained in BGR.
             frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             #Make estimation of poses 
             results = poseVideo.process(frameRGB)
@@ -103,31 +94,23 @@ def retrievePoseEstimates(videoPath):
             if(results.pose_landmarks):
                 #Iterate through each landmark and take its x and y cordinates
                 for _,lm in enumerate(results.pose_landmarks.landmark):
-                    #Appending x cordinate to the pose_x list
                     pose_x.append(lm.x)
-                    #Appending y cordinate to the pose_y list
                     pose_y.append(lm.y)
             #if pose landmark does not exist- Fill in NaN to both the list
             else:
                 pose_x.append('NaN')
                 pose_y.append('NaN')
-            #x and y cordinates of each estimated right hand landmark i.e., results.right_hand_landmarks.landmark
             if(results.right_hand_landmarks):
-                #Iterate through each landmark and take its x and y cordinates
                 for _,lm in enumerate(results.right_hand_landmarks.landmark):
                     right_hand_x.append(lm.x)
                     right_hand_y.append(lm.y)
-            #if it does not exist- Fill in NaN to both the list
             else:
                 right_hand_x.append('NaN')
                 right_hand_y.append('NaN')
-            #x and y cordinates of each estimated left hand landmark i.e., results.left_hand_landmarks.landmark
             if(results.left_hand_landmarks):
-                #Iterate through each landmark and take its x and y cordinates
                 for _,lm in enumerate(results.left_hand_landmarks.landmark):
                     left_hand_x.append(lm.x)
                     left_hand_y.append(lm.y)
-            #if it does not exist- Fill in NaN to both the list
             else:
                 left_hand_x.append('NaN')
                 left_hand_y.append('NaN')
